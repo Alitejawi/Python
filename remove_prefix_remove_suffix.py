6a21a97aa25d1f039ddd08ffc517b80db4ab364b
Ali Al-Itejawi <ali@al-itejawi.com>
2020-12-10 22:10:33 +0100

    Small change to remove_prefix_remove_suffix.py

---
+++ remove_prefix_remove_suffix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+__author__ = "Ali Al-Itejawi"
+__version__ = "1.1"
+__email__ = "ali@al-itejawi.com"
+
+__description__ = "As of Python3.9, there Python3 has a new string method available to remove and add suffixes and prefixes"
+
+str = 'antisocial'
+print(str.removeprefix('anti'))
+
+str = 'ownership'
+print(str.removesuffix('ship'))
